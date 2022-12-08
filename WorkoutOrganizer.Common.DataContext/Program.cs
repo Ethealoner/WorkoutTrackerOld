ae1d990c3a4f26ad680cf4787f5bc5f81e9445cc
Ethealoner <ethealoner@gmail.com>
2022-12-08 22:58:54 +0100

    First commit for the workout tracker app

---
+++ WorkoutOrganizer.Common.DataContext/Program.cs
@@ -0,0 +1,24 @@
+﻿using WorkoutOrganizer.Common.DataContext;
+using WorkoutOrganizer.Common.DataEntity;
+
+static void ViewBasicTables()
+{
+    using (WorkoutDatabase db = new())
+    {
+        
+        IQueryable<Exercise>? exercise = db.Exercises;
+
+        if (exercise is null)
+        {
+            Console.WriteLine("No exercises found");
+            return;
+        }
+
+        foreach(var exercise2 in exercise)
+        {
+            Console.WriteLine($"{exercise2.Name}, {exercise2.Id}, {exercise2.TypeOfExercise}");
+        }
+    }
+}
+
+ViewBasicTables();
---
+++ WorkoutOrganizer.Common.DataEntity/ExercisesList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WorkoutOrganizer.Common.DataEntity;
+
+[Table("ExercisesList")]
+public class ExercisesList
+{
+    [Key]
+    public int ExercisesId { get; set; }
+
+    public int WorkoutSessionId { get; set; }
+
+    [InverseProperty("ExerciseNavigation")]
+    public virtual ICollection<Exercise> Exercises { get; } = new List<Exercise>();
+
+    [ForeignKey("WorkoutSessionId")]
+    [InverseProperty("ExercisesLists")]
+    public virtual WorkoutSession WorkoutSession { get; set; } = null!;
+}
