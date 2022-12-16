45e7c10584016db9edd9fa7b77f678eba6c25c93
Ethealoner <ethealoner@gmail.com>
2022-12-16 20:54:36 +0100

    Add CRUD operations for workoutSession entity

--- WorkoutOrganizer.Common.WebAPI/Controllers/WorkoutSessionController.cs
+++ WorkoutOrganizer.Common.WebAPI/Controllers/WorkoutSessionController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WorkoutTracker.Common.DataContext;
+using WorkoutTracker.Common.DataEntity;
 
 namespace WorkoutTracker.Common.WebAPI.Controllers
 {
@@ -19,6 +20,60 @@ namespace WorkoutTracker.Common.WebAPI.Controllers
         {
             var workoutSessions = await workoutDatabase.WorkoutSessions.ToListAsync();
             return Ok(workoutSessions);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddWorkoutSessions([FromBody] WorkoutSession workoutSessions)
+        {
+            await workoutDatabase.WorkoutSessions.AddAsync(workoutSessions);
+
+            workoutDatabase.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> UpdateExercise([FromRoute] int id, WorkoutSession updateWorkoutSessions)
+        {
+            WorkoutSession workoutSessions = await workoutDatabase.WorkoutSessions.FindAsync(id);
+
+            if (workoutSessions == null)
+            {
+                return NotFound();
+            }
+            workoutSessions.WorkoutScore = updateWorkoutSessions.WorkoutScore;
+            workoutSessions.WorkoutDate = updateWorkoutSessions.WorkoutDate;
+            await workoutDatabase.SaveChangesAsync();
+
+            return Ok(workoutSessions);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetExercises([FromRoute] int id)
+        {
+            var workoutSessions = await workoutDatabase.WorkoutSessions.FirstOrDefaultAsync(x => x.WorkoutSessionId == id);
+
+            if (workoutSessions == null)
+            {
+                return NotFound();
+            }
+            return Ok(workoutSessions);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteExercise([FromRoute] int id)
+        {
+            var workoutSessions = await workoutDatabase.WorkoutSessions.FirstOrDefaultAsync(x => x.WorkoutSessionId == id);
+
+            if (workoutSessions == null)
+            {
+                return NotFound();
+            }
+            workoutDatabase.WorkoutSessions.Remove(workoutSessions);
+            await workoutDatabase.SaveChangesAsync();
+            return Ok();
+
         }
     }
 }
